9ca65a197286cd627d152c830333f415dfffde85
Rob Hitt <robhitt@NYC-Rob-Hitt-ENG.local>
2017-11-28 16:26:08 -0500

    Update styling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Key Code Viewer
-Find the **key code** of any key  
-[@robhitt](//instagram.com/robhitt)
+# -Key Code Viewer-
+## [robhitt.com/keycode](//www.robhitt.com/keycode)  
+#### Find the **key code** of any key  
+#### [@robhitt](//instagram.com/robhitt)
--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  window.addEventListener('keydown', pressedKey);
+
+  let key = document.querySelector(".key");
+  let keyCode = document.querySelector(".key-code");
+  key.style.display = "none";
+
+  function pressedKey(event) {
+    console.log(`${event.key} `, event.keyCode);
+    document.querySelector(".welcome").style.display = "none";
+    key.style.display = "initial";
+    keyCode.textContent = event.keyCode;
+    key.textContent = event.key;
+  }
 
 });
